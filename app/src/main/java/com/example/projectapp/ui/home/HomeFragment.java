fcb4a0d630c469361775504e75f42a767837e288
joonaun <joonas.niemi@metropolia.fi>
2019-12-02 09:50:15 +0200

    v0.1.7 paranneltu tuote haku. Ei kaadu

--- app/src/main/java/com/example/projectapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/projectapp/ui/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.projectapp.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -24,7 +25,8 @@ import com.example.projectapp.filehandler.FileReader;
 import com.example.projectapp.food_stuff.Food;
 import com.example.projectapp.food_stuff.FoodList;
 import com.example.projectapp.ui.MainActivity;
-import com.example.projectapp.ui.info.InfoFragment;
+import com.example.projectapp.ui.home.foodinfo.FoodInfoActivity;
+import com.example.projectapp.ui.home.foodinfo.InfoFragment;
 
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -32,9 +34,10 @@ import java.util.List;
 
 import static android.content.Context.INPUT_METHOD_SERVICE;
 
-public class HomeFragment extends Fragment implements View.OnClickListener {
-
-    public static final int MYFILE = R.raw.resultset;
+public class HomeFragment extends Fragment implements View.OnClickListener, AdapterView.OnItemClickListener {
+    public static final String EXTRA = "com.example.projectapp.ui.home.EXTRA";
+    private static final String TAG = "MyLog";
+    private static final int MYFILE = R.raw.resultset;
     private List ruokalista;
     private ListView lvFoods;
     private ArrayList<Food> copy;
@@ -47,32 +50,12 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
         lvFoods = root.findViewById(R.id.lvFoods);
         setHasOptionsMenu(true);
         showSearch = false;
-        et = getActivity().findViewById(R.id.ptFind);
 
         Button searchButton = root.findViewById(R.id.bSearch);
         searchButton.setOnClickListener(this);
+        lvFoods.setOnItemClickListener(this);
 
         ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(false);
-
-        //Show food information
-        lvFoods.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Log.i("testilog", "home " + i);
-
-                searchWords = et.getText().toString();
-
-                Bundle bundle = new Bundle();
-                bundle.putInt("item", copy.get(i).getId());
-                Fragment infoFragment = new InfoFragment();
-                infoFragment.setArguments(bundle);
-
-                FragmentTransaction ft = getFragmentManager().beginTransaction();
-                ft.replace(R.id.nav_host_fragment, infoFragment);
-                ft.addToBackStack(null);
-                ft.commit();
-            }
-        });
         return root;
     }
 
@@ -81,28 +64,46 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.bSearch:
-                Log.i("testilog", "search button toimii");
+                Log.i(TAG, "search button toimii");
+                try {
+                    InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(getActivity().getCurrentFocus().getWindowToken(), 0);
+                } catch (Exception e) {
+                    Log.d(TAG, "keyboard not visible");
+                }
+                getActivity().findViewById(R.id.ptFind).clearFocus();
+
                 if (et.getText().toString().isEmpty()) {
                     if (copy.size() < FoodList.getInstance().getFoods().size()) {
                         searchFoods();
                     }
-                    try {
-                        InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(INPUT_METHOD_SERVICE);
-                        imm.hideSoftInputFromWindow(getActivity().getCurrentFocus().getWindowToken(), 0);
-                    } catch (Exception e) {
-                        Log.d("Error", "keyboard not visible");
-                    }
                 } else {
                     searchFoods();
-                    try {
-                        InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(INPUT_METHOD_SERVICE);
-                        imm.hideSoftInputFromWindow(getActivity().getCurrentFocus().getWindowToken(), 0);
-                    } catch (Exception e) {
-                        Log.d("Error", "keyboard not visible");
-                    }
                 }
                 break;
         }
+    }
+
+    //ArrayList item click listener
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int i, long l) {
+        Log.i(TAG,  "Item ID: " + copy.get(i).getId());
+        searchWords = et.getText().toString();
+
+        Intent infoIntent = new Intent(getActivity(), FoodInfoActivity.class);
+        infoIntent.putExtra(EXTRA, copy.get(i).getId());
+        startActivity(infoIntent);
+
+        /*
+        Bundle bundle = new Bundle();
+        bundle.putInt("item", copy.get(i).getId());
+        Fragment infoFragment = new InfoFragment();
+        infoFragment.setArguments(bundle);
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.replace(R.id.nav_host_fragment, infoFragment);
+        ft.addToBackStack(null);
+        ft.commit();
+         */
     }
 
     //After view is created
@@ -112,10 +113,10 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
         et.setText(searchWords);
         searchFoods();
         if(((MainActivity)getActivity()).getSearchVisible()) {
-            getView().findViewById(R.id.ptFind).setVisibility(View.VISIBLE);
+            et.setVisibility(View.VISIBLE);
             getView().findViewById(R.id.bSearch).setVisibility(View.VISIBLE);
         } else{
-            getView().findViewById(R.id.ptFind).setVisibility(View.GONE);
+            et.setVisibility(View.GONE);
             getView().findViewById(R.id.bSearch).setVisibility(View.GONE);
         }
     }
@@ -128,11 +129,13 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
 
     // Show all foods or find specific one
     private void searchFoods() {
-        //Read food file
-        FileReader reader = new FileReader();
-        InputStream myFile = getActivity().getResources().openRawResource(MYFILE);
-        ruokalista = reader.readFile(myFile);
-        FoodList.getInstance().addFoods(ruokalista);
+        // If FoodList is empty, create new list
+        if(FoodList.getInstance().getFoods().size() == 0){
+            FileReader reader = new FileReader();
+            InputStream myFile = getActivity().getResources().openRawResource(MYFILE);
+            ruokalista = reader.readFile(myFile);
+            FoodList.getInstance().addFoods(ruokalista);
+        }
 
         //Food you want to find
         EditText findText = getActivity().findViewById(R.id.ptFind);
---
+++ app/src/main/java/com/example/projectapp/ui/home/foodinfo/FoodInfoActivity.java
@@ -0,0 +1,93 @@
+package com.example.projectapp.ui.home.foodinfo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NavUtils;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.example.projectapp.R;
+import com.example.projectapp.food_stuff.Food;
+import com.example.projectapp.food_stuff.FoodList;
+import com.example.projectapp.ui.AppInfoActivity;
+import com.example.projectapp.ui.MainActivity;
+
+public class FoodInfoActivity extends AppCompatActivity {
+    private static final String TAG = "MyLog";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_info);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Bundle b = getIntent().getExtras();
+        int value = b.getInt(MainActivity.EXTRA, 0);
+        Food food = FoodList.getInstance().getFoods().get(value);
+        Log.i(TAG, "info " + value);
+
+        ((TextView)findViewById(R.id.tvFoodName))
+                .setText(food.getName());
+        ((TextView)findViewById(R.id.tvEnergia))
+                .setText(food.getEnergia() + "kJ/ " + Math.round(Integer.parseInt(food.getEnergia()) / 4.1868) + "kcal");
+        ((TextView)findViewById(R.id.tvHiilihydraatit))
+                .setText(food.getHiilihyraatti() + "g");
+        ((TextView)findViewById(R.id.tvKuitu))
+                .setText(food.getKuitu() + "g");
+        ((TextView)findViewById(R.id.tvProteiini))
+                .setText(food.getProteiini() + "g");
+        ((TextView)findViewById(R.id.tvRasva))
+                .setText(food.getRasva() + "g");
+        ((TextView)findViewById(R.id.tvSuola))
+                .setText(food.getSuola() + "mg");
+    }
+
+    //Create action bar buttons
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action_bar_items, menu);
+        menu.findItem(R.id.action_search).setVisible(false);
+        return true;
+    }
+
+    //Options menu item click events
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_info:
+                Log.i(TAG, "action bar appInfo toimii");
+                Intent intent = new Intent(this, AppInfoActivity.class);
+                startActivity(intent);
+                return true;
+
+            case R.id.home:
+                Log.i(TAG, "action bar back toimii");
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        Log.i(TAG, "foodInfoActivity actionBar back toimii");
+        super.onBackPressed();
+        return super.onSupportNavigateUp();
+    }
+
+    @Override
+    public void onBackPressed() {
+        Log.i(TAG, "foodInfoActivity back toimii");
+        super.onBackPressed();
+    }
+}
