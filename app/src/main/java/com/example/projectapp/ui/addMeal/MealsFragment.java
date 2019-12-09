4c6deca6e59319599a559805a8178c283ae97121
mirono <mirono@metropolia.fi>
2019-12-09 14:08:17 +0200

    valmiiden aterioiden lista luotu epic victory royale v2

--- app/src/main/java/com/example/projectapp/ui/addMeal/MealsFragment.java
+++ app/src/main/java/com/example/projectapp/ui/addMeal/MealsFragment.java
@@ -1,8 +1,11 @@
 package com.example.projectapp.ui.addMeal;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
 import android.view.ContextMenu;
 import android.view.LayoutInflater;
@@ -23,6 +26,7 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -44,7 +48,7 @@ import static android.content.Context.INPUT_METHOD_SERVICE;
 /**
  * Class that contains MealsFragment
  */
-public class MealsFragment extends Fragment {
+public class MealsFragment extends Fragment implements AdapterView.OnItemClickListener {
     private static final String TAG = "MyLog";
     private static final int MYFILE = R.raw.resultset;
     private ListView lvSavedMeals;
@@ -80,7 +84,7 @@ public class MealsFragment extends Fragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         lvSavedMeals = getView().findViewById(R.id.lvSavedMeals);
-
+        registerForContextMenu(lvSavedMeals);
         showMeals();
     }
 
@@ -90,9 +94,31 @@ public class MealsFragment extends Fragment {
                 meals));
     }
 
-    public void updateUI() {
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        switch (item.getItemId()) {
+            case R.id.delete:
+                Log.i(TAG, "Poisto toimii");
+                Log.i(TAG, "Long click id " + info.id);
+                meals.remove((int) info.id);
+                showMeals();
+                return true;
+        }
+        return super.onContextItemSelected(item);
+    }
 
-        showMeals();
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        if (v.getId() == R.id.lvSavedMeals) {
+            MenuInflater inflater = getActivity().getMenuInflater();
+            inflater.inflate(R.menu.menu_list, menu);
+        }
+    }
 
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+      
     }
 }
