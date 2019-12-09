662f1f9923c26ed969b3a10358b6ada9ef276c74
elmerivk@metropolia.fi <3Lkkupompp1192>
2019-12-09 14:48:31 +0200

    paskaa

--- app/src/main/java/com/example/projectapp/ui/history/AddFoodToHistory.java
+++ app/src/main/java/com/example/projectapp/ui/history/AddFoodToHistory.java
@@ -2,19 +2,67 @@ package com.example.projectapp.ui.history;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.projectapp.R;
+import com.example.projectapp.food_stuff.Meal;
+import com.example.projectapp.food_stuff.MealsList;
+import com.example.projectapp.ui.AppInfoActivity;
 
-public class AddFoodToHistory extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class AddFoodToHistory extends AppCompatActivity implements Spinner.OnItemSelectedListener{
 
     private static final String TAG = "MyLog";
+    private String[] mealNames;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_food_to_history);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+
+
+
+    }
+
+    /**@Override
+    protected void onStart() {
+
+        Spinner mealList = findViewById(R.id.spinner);
+        mealList.setOnItemSelectedListener(this);
+
+        for (int i = 0; i < MealsList.getInstance().getMeals().size(); i++) {
+            mealNames[i] = (MealsList.getInstance().getMeals().get(i).getMeal().toString());
+        }
+        //Creating the ArrayAdapter instance having the meal list
+        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item, mealNames);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Setting the ArrayAdapter data on the Spinner
+        mealList.setAdapter(aa);
+        super.onStart();
+    }*/
+
+    //Performing action onItemSelected and onNothing selected
+    @Override
+    public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id) {
+        Toast.makeText(getApplicationContext(),mealNames[position] , Toast.LENGTH_LONG).show();
+    }
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
     }
 
     @Override
@@ -27,7 +75,27 @@ public class AddFoodToHistory extends AppCompatActivity {
     public void onBackPressed() {
         Log.i(TAG, "takaisin ateriafragmenttiin");
         super.onBackPressed();
-
-
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action_bar_items, menu);
+        menu.findItem(R.id.action_search).setVisible(false);
+        menu.findItem(R.id.action_info).setVisible(false);
+        return true;
+    }
+
+    /**@Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_addMeal:
+                Log.i(TAG, "action bar appInfo toimii");
+                Intent intent = new Intent(this, AppInfoActivity.class);
+                startActivity(intent);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }*/
+
+
 }
--- app/src/main/java/com/example/projectapp/ui/history/HistoryFragment.java
+++ app/src/main/java/com/example/projectapp/ui/history/HistoryFragment.java
@@ -1,24 +1,29 @@
 package com.example.projectapp.ui.history;
 
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.CalendarView;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.example.projectapp.R;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class HistoryFragment extends Fragment {
+
+    private static final String TAG = "MyLog";
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_history, container, false);
@@ -29,6 +34,18 @@ public class HistoryFragment extends Fragment {
             public void onClick(View v) {
                 Intent addFood = new Intent(getActivity(), AddFoodToHistory.class);
                 startActivity(addFood);
+                //startActivity(new Intent(PopUp.class)); popup alku
+            }
+        });
+
+        CalendarView calendarView = root.findViewById(R.id.calendarView);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+
+                Calendar calendar = Calendar.getInstance();
+                calendar.set(year, month, dayOfMonth);
+                //Log.i(TAG, "" + calendar.get(Calendar.DAY_OF_MONTH));
             }
         });
         return root;
@@ -40,5 +57,12 @@ public class HistoryFragment extends Fragment {
         super.onCreateOptionsMenu(menu, inflater);
         menu.findItem(R.id.action_search).setVisible(false);
         menu.findItem(R.id.action_addMeal).setVisible(false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        CalendarView cv = getView().findViewById(R.id.calendarView);
+        cv.setMaxDate(new Date().getTime());
     }
 }
